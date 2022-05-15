350f49c7a588de23f88f845d934b6b16b05e2592
coder-18m <76606410+coder-18m@users.noreply.github.com>
2022-05-15 12:49:45 -0700

    debugging nav javascript

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-window.addEventListener('mouseup', function(){
+window.addEventListener('touchStart', function(){
   var box = document.getElementById('ul-li-ul');
   if (event.target != box && event.target.parentNode != box) {
     box.style.display = 'none';
