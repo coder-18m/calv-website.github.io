d6a997be8ba87aa4cd9df3360b62b74f02d39579
coder-18m <76606410+coder-18m@users.noreply.github.com>
2022-05-15 13:21:34 -0700

    more debugging of nav bar js function

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-window.addEventListener('touchStart', function(event){
+window.addEventListener('touchstart', function(event){
   var box = document.getElementById('ul-li-ul');
   if (event.target != box && event.target.parentNode != box) {
     box.style.display = 'none';
