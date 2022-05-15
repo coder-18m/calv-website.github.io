dcfd39a917ff36a19aa65c855d04ea0096db8490
coder-18m <76606410+coder-18m@users.noreply.github.com>
2022-05-15 12:44:02 -0700

    trying to close nav dropdown on ipad

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
-$(document).click(function (event) {
-    var clickover = $(event.target);
-    var $navbar = $(".ul-li-ul");
-    var _opened = $navbar.hasClass("in");
-    if (_opened === true && !clickover.hasClass(".ul-li-ul")) {
-        $navbar.collapse('hide');
-    }
+window.addEventListener('mouseup', function(){
+  var box = document.getElementById('ul-li-ul');
+  if (event.target != box && event.target.parentNode != box) {
+    box.style.display = 'none';
+  }
 });
