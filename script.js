6ea1aa016712144c2e4d116b9748f7cd05640d58
coder-18m <76606410+coder-18m@users.noreply.github.com>
2022-05-15 13:10:40 -0700

    navbar closing on mobile js debugging by adding event variable

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-window.addEventListener('touchStart', function(){
+window.addEventListener('touchStart', function(event){
   var box = document.getElementById('ul-li-ul');
   if (event.target != box && event.target.parentNode != box) {
     box.style.display = 'none';
