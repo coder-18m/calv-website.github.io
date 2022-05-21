d85e6ef8eab0ad1a56c41580099a35e60bddfe10
coder-18m <76606410+coder-18m@users.noreply.github.com>
2022-05-21 03:30:24 -0700

    updated js file, add smooth link to sample letter page

--- script.js
+++ script.js
@@ -13,3 +13,33 @@ aboutLink.addEventListener('click', navigateItem, false);
 function navigateItem(e){
 aboutItem.scrollIntoView({behavior: "smooth"});
 }
+
+// talking points smooth link
+let talkLink = document.querySelector('#talk-link');
+let talkItem = document.querySelector('#talk-link-b');
+
+talkLink.addEventListener('click', navigateItem2, false);
+
+function navigateItem2(e){
+talkItem.scrollIntoView({behavior: "smooth"});
+}
+
+// template letter smooth link
+let tempLink = document.querySelector('#temp-link');
+let tempItem = document.querySelector('#temp-link-b');
+
+tempLink.addEventListener('click', navigateItem3, false);
+
+function navigateItem3(e){
+tempItem.scrollIntoView({behavior: "smooth"});
+}
+
+//contact info smooth link
+let contactLink = document.querySelector('#contact-link');
+let contactItem = document.querySelector('#contact-link-b');
+
+contactLink.addEventListener('click', navigateItem4, false);
+
+function navigateItem4(e){
+contactItem.scrollIntoView({behavior: "smooth"});
+}
